aaf15e5c8351aef7d971a936b915e907e3d01956
Amy Jacobs <amelia.b.jacobs@gmail.com>
2017-11-16 21:53:05 -0800

    update program to accurately fit project guidelines

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-var pingArray = []
 
 var pingPong = function(input) {
-  if (input%15 === 0) {
-    return "pingpong";
-  } else if (input%5 === 0) {
-    return "pong";
-  } else if (input%3 === 0) {
-    return "ping";
-  } else {
-    pingArray = []
-    for (var index = input; index >= 1; index -= 1) {
+  var pingArray = []
+  for (var index = input; index >= 1; index -= 1) {
       pingArray.push(index + " ");
-    }
-    return pingArray;
   }
+  console.log(pingArray);
+
+  for (var index = 0; index < pingArray.length; index += 1) {
+    if (pingArray[index]%15 === 0) {
+      pingArray[index] = "pingpong ";
+    } else if (pingArray[index]%5 === 0 ) {
+      pingArray[index] = "pong ";
+    } else if (pingArray[index]%3 === 0){
+      pingArray[index] =  "ping ";
+    }
+  }
+  return pingArray;
+  // if (input%15 === 0) {
+  //   return "pingpong";
+  // } else if (input%5 === 0) {
+  //   return "pong";
+  // } else if (input%3 === 0) {
+  //   return "ping";
+  // } else {
+  //   pingArray = []
+  //   for (var index = input; index >= 1; index -= 1) {
+  //     pingArray.push(index + " ");
+  //   }
+  //   return pingArray;
+  // }
 };
 
 $(document).ready(function() {
